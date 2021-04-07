f124678dd9ded604721379984f4ce20c06c7aafe
MrBreadnt <71169288+MrBreadnt@users.noreply.github.com>
2021-04-07 14:55:13 +0700

    Пофикшена панель навигации

--- app/src/main/java/ru/breadntgames/ktulhutamagochi/MainActivity.java
+++ app/src/main/java/ru/breadntgames/ktulhutamagochi/MainActivity.java
@@ -39,4 +39,17 @@ public class MainActivity extends AppCompatActivity {
         happyProgress = findViewById(R.id.happy_progress);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
